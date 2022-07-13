6c0b030ede65f53df88ee5145ed7fa096fc68605
darkdevil3610 <94959564+darkdevil3610@users.noreply.github.com>
2022-07-13 20:43:38 +0530

    Update random-sound.js

--- plugins/random-sound.js
+++ plugins/random-sound.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command }) => {
 m.reply('Wait 🐦')
 let audio = `https://github.com/saipulanuar/Api-Github/tree/main/audio/${command}.mp3`
-await conn.sendFile(m.chat, audio, 'error.mp3', null, m, true)
+await conn.sendFile(m.chat, audio, 'gourav.ppt', null, m, true)
 }
 
 handler.command = handler.help = [
