ae390a391bc3c87d010a8b085943334d41042170
dark_devil_3609 <94959564+darkdevil3610@users.noreply.github.com>
2023-02-04 21:43:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ global.loadDatabase = async function loadDatabase() {
 loadDatabase()
 
 global.authFile = `${opts._[0] || 'session'}.data.json`
-const { state, saveState } = useSingleFileAuthState(global.authFile)
+//const { state, saveState } = useSingleFileAuthState(global.authFile)
 
 const connectionOptions = {
   printQRInTerminal: true,
